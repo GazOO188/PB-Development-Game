ca26b4e18b6c81b0273925707e3246416c553032
O-ZZ-EY <o.tavarez8902@gmail.com>
2026-03-11 15:18:15 -0400

    Updated
    .

--- Assets/Scripts Folder/AudioScripts/ClickSound.cs
+++ Assets/Scripts Folder/AudioScripts/ClickSound.cs
@@ -4,13 +4,23 @@ public class ClickSound : MonoBehaviour
 {
     [SerializeField] private SoundLibrary sounds;
 
-    public void PlayClick()
+    public void PlayClickSound1()
     {
-        var entry = sounds.Get("ui_click");
+        var entry = sounds.Get("howtoplay_click");
         AudioManager.Instance.PlaySFX(
             entry.variants[Random.Range(0, entry.variants.Length)], //AudioClip
             entry.volume
-            //Pitch not passed to defaults to 1.0f.
+        //Pitch not passed to defaults to 1.0f.
+        );
+    }
+
+    public void PlayClickSound2()
+    {
+        var entry = sounds.Get("settings_click");
+        AudioManager.Instance.PlaySFX(
+            entry.variants[Random.Range(0, entry.variants.Length)], //AudioClip
+            entry.volume
+        //Pitch not passed to defaults to 1.0f.
         );
     }
 }
