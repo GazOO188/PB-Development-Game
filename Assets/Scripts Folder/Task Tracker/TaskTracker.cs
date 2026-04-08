9bd8c450d13f95106bbcaf893cc58bd6e9f47b72
jpiza8929 <juliuspizarro11@gmail.com>
2026-04-08 14:26:19 -0400

    Fixed SprayFoam To not Work

---
+++ Assets/Scripts Folder/Task Tracker/TaskTracker.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class TaskTracker : MonoBehaviour
+{
+    //THIS SCRIPT IS FOR TRACKING THE ELECTRICAL TASKS//
+
+     [Header("Scripts References")]
+
+     public WorkPhaseTimer WPT;
+
+    
+    
+    // public Dictionary<
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
