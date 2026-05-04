12b35ac060353d8f9d2868928aa46d2396944662
Manny Patrick Vivas <mannyvivas21@gmail.com>
2026-05-04 02:08:49 -0400

    updated work scene
    - curtain wind feedback
    - new outline feedback for circuit breakers
    - new outline feedback for outlet
    - outlet tester now required for objective
    - inventory UI update

--- Assets/Scripts Folder/Tools/Outlet.cs
+++ Assets/Scripts Folder/Tools/Outlet.cs
@@ -8,15 +8,25 @@ public class Outlet : MonoBehaviour
     public List<GameObject> outlets = new List<GameObject>();
     int index = 0;
     public bool active = true;
+    public bool canBeTested = false;
+    public bool outletTested = false;
     public bool complete = false;
+    Outline outline;
     void Start()
     {
         for (int i = 0; i < outlets.Count; i++)
             outlets[i].GetComponent<Renderer>().material.color = Color.gray;
+
+        outline = GetComponentInChildren<Outline>();
+        outline.enabled = false;
     }
 
     void Update()
     {
+        if (timer.TaskOneDisplayed && !canBeTested) canBeTested = true;
+
+        UpdateOutline();
+
         active = PlayerInventory.Instance.currentItem != null &&
                  PlayerInventory.Instance.currentItem.itemName == "Outlet";
 
@@ -31,13 +41,48 @@ public class Outlet : MonoBehaviour
         }
         else
         {
-            if (complete) Debug.Log("YOU DID IT");
+            //if (complete) Debug.Log("YOU DID IT");
+        }
+    }
+
+    void UpdateOutline()
+    {
+        if (PlayerInventory.Instance.currentItem == null || complete || !timer.TaskOneDisplayed)
+        {
+            if (outline.enabled) outline.enabled = false;
+            return;
+        }
+
+        if (!outletTested)
+        {
+            if (PlayerInventory.Instance.currentItem.itemName == "Outlet Tester")
+            {
+                if (!outline.enabled) outline.enabled = true;
+                if (outline.OutlineColor != Color.green) outline.OutlineColor = Color.green;
+            }
+            else
+            {
+                if (outline.enabled) outline.enabled = false;
+            }
+        }
+        else
+        {
+            if (PlayerInventory.Instance.currentItem.itemName == "Outlet")
+            {
+                if (!outline.enabled) outline.enabled = true;
+                if (outline.OutlineColor != Color.green) outline.OutlineColor = Color.green;
+            }
+            else
+            {
+                if (outline.enabled) outline.enabled = false;
+            }
+
         }
     }
 
     public void UpdateOutlet(GameObject currentOutlet)
     {
-        if (!timer.TaskOneDisplayed) return;
+        if (!timer.TaskOneDisplayed || !outletTested) return;
 
         foreach (GameObject outlet in outlets)
         {
@@ -53,7 +98,7 @@ public class Outlet : MonoBehaviour
         {
             PlayerInventory.Instance.currentItem = null;
             complete = true;
-            Debug.Log("Did");
+            //Debug.Log("Did");
         }
     }
 }
